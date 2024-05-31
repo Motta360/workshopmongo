c7b215e575caad00ec56cfa0c987feae70b7966d
Motta360 <lucas-mottateixeira@hotmail.com>
2024-05-31 16:11:54 -0300

    UserDTO

--- src/main/java/com/lucasmotta/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lucasmotta/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.lucasmotta.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucasmotta.workshopmongo.domain.User;
+import com.lucasmotta.workshopmongo.dto.UserDTO;
 import com.lucasmotta.workshopmongo.services.UserService;
 
 @RestController
@@ -17,10 +19,11 @@ public class UserResource {
 	@Autowired
 	private UserService userService;
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> lista = userService.findAll();
+		List<UserDTO> lista2 = lista.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		
-		return ResponseEntity.ok().body(lista);
+		return ResponseEntity.ok().body(lista2);
 		
 	}
 
